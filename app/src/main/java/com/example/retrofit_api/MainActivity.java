6ff71b30061d501358e810fcebdbac35b615340d
Tienanh1904 <tienanhcyb1904990@gmail.com>
2019-10-22 17:43:18 +0700

    Retrofit_Android

---
+++ app/src/main/java/com/example/retrofit_api/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.retrofit_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.retrofit_api.data.model.model.Item;
+import com.example.retrofit_api.data.model.model.SOAnswersResponse;
+import com.example.retrofit_api.data.model.remote.SOService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private AnswersAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    private SOService mService;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mService = ApiUtils.getSOService();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycleView);
+        mAdapter = new AnswersAdapter(this, new ArrayList<Item>(0), new AnswersAdapter.PostItemListener() {
+
+            @Override
+            public void onPostClick(long id) {
+                Toast.makeText(MainActivity.this, "Post id is" + id, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(itemDecoration);
+
+        loadAnswers();
+    }
+
+    public void loadAnswers() {
+        mService.getAnswers().enqueue(new Callback<SOAnswersResponse>() {
+            @Override
+            public void onResponse(Call<SOAnswersResponse> call, Response<SOAnswersResponse> response) {
+                if(response.isSuccessful()){
+                    mAdapter.updateAnswers(response.body().getItems());
+                    Log.d("MainActivity","post loaded from API");
+                }
+                else {
+                    int statusCode = response.code();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SOAnswersResponse> call, Throwable t) {
+                Log.d("MainActivity", "error loading from API");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/retrofit_api/data/model/remote/SOService.java
@@ -0,0 +1,25 @@
+package com.example.retrofit_api.data.model.remote;
+
+import com.example.retrofit_api.data.model.model.SOAnswersResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface SOService {
+
+        /*
+                @Path biến thay thế cho API endpoint
+                @Query	xác định tên khoá truy vấn với giá trị của tham số được chú thích
+                @Body	payload cho việc gọi POST
+                @Header	thiết lập header với giá trị của tham số được chú thích
+         */
+
+        @GET("/answers?order=desc&sort=activity&site=stackoverflow")
+        Call<SOAnswersResponse> getAnswers();
+
+        @GET("/answers?order=desc&sort=activity&site=stackoverflow")
+        Call<SOAnswersResponse> getAnswers(@Query("tagged") String tags);
+}
